7afa0a7f014476a348814ea90700955421954d97
whoyoux <48690153+whoyoux@users.noreply.github.com>
2022-08-01 14:42:46 +0200

    Removed cert file and moved to .env

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,12 +1,14 @@
 import * as firebaseAdmin from 'firebase-admin';
-import serviceAccount from './cred.json';
 
 if (!firebaseAdmin.apps.length) {
     firebaseAdmin.initializeApp({
         credential: firebaseAdmin.credential.cert({
-            privateKey: serviceAccount.private_key,
-            clientEmail: serviceAccount.client_email,
-            projectId: serviceAccount.project_id
+            privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(
+                /\\n/g,
+                '\n'
+            ),
+            clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+            projectId: process.env.FIREBASE_ADMIN_PROJECT_ID
         }),
         databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
     });
